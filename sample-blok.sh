6c5e22c39d588fb56dfc3139c24aa00da0439187
jendralhxr <jendral.hxr@gmail.com>
2013-06-23 13:28:32 +0700

    rename montice -> blokice (duh!)

--- sample-blok.sh
+++ sample-blok.sh
@@ -5,7 +5,7 @@ do
 for try in 1 2 3
 do
 echo +++++++++ try num= "$try" >> samples-blok-a
-./montice "pow(x*x+y*y+z*z,1.5)" -3 3 $sample "-1*pow(9-x*x,0.5)" "pow(9-x*x,0.5)" $sample "-1*pow(9-x*x-y*y,0.5)" "pow(9-x*x-y*y,0.5)" $sample 24 hostfiles/host_cluster >> samples-blok-a
+./blokice "pow(x*x+y*y+z*z,1.5)" -3 3 $sample "-1*pow(9-x*x,0.5)" "pow(9-x*x,0.5)" $sample "-1*pow(9-x*x-y*y,0.5)" "pow(9-x*x-y*y,0.5)" $sample 24 hostfiles/host_cluster >> samples-blok-a
 done
 done
 
@@ -14,7 +14,7 @@ do
 for try in 1 2 3
 do
 echo +++++++++ try num= "$try" >> samples-blok-b
-./montice "y/(y*y+z*z)" 1 2 $sample x 3 $sample 0 "y*pow(3,0.5)" $sample 24 hostfiles/host_cluster >> samples-blok-b
+./blokice "y/(y*y+z*z)" 1 2 $sample x 3 $sample 0 "y*pow(3,0.5)" $sample 24 hostfiles/host_cluster >> samples-blok-b
 done
 done
 
@@ -23,7 +23,7 @@ do
 for try in 1 2 3
 do
 echo +++++++++ try num= "$try" >> samples-blok-c
-./montice "pow(x*x+y*y,0.5)" 0 3 $sample 0 "pow(9-x*x,0.5)" $sample 0 2 $sample 24 hostfiles/host_cluster >> samples-blok-c
+./blokice "pow(x*x+y*y,0.5)" 0 3 $sample 0 "pow(9-x*x,0.5)" $sample 0 2 $sample 24 hostfiles/host_cluster >> samples-blok-c
 done
 done
 
@@ -32,6 +32,6 @@ do
 for try in 1 2 3 
 do
 echo +++++++++ try num= "$try" >> samples-blok-d
-./montice "z*pow(4-x*x-y*y,0.5)" 0 2 $sample 0 "pow(4-x*x,0.5)" $sample 0 "pow(4-x*x-y*y,0.5)" $sample 24  hostfiles/host_cluster >> samples-blok-d
+./blokice "z*pow(4-x*x-y*y,0.5)" 0 2 $sample 0 "pow(4-x*x,0.5)" $sample 0 "pow(4-x*x-y*y,0.5)" $sample 24  hostfiles/host_cluster >> samples-blok-d
 done
 done
